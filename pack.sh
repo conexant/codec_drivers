b8b2693a4cf0bc0587555a9d0861ea20072114c1
Aimar.Liu <aimar.liu@conexant.com>
2017-06-16 15:38:51 +0800

    ASoC: add pack.sh to package deployed files
    Change-Id: If32beecd99f19391629ffc3bcc2a2c733dea06f5
    Signed-off-by: Aimar.Liu <aimar.liu@conexant.com>

---
+++ pack.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DIR=$PWD
+image="zImage"
+vmlinuz="vmlinuz"
+KERNEL_ARCH=arm
+CC=arm-linux-gnueabi-
+mkdir -p "${DIR}/deploy/boot/dtbs/"
+
+# copy kernel release version to version
+cp ${DIR}/include/config/kernel.release ${DIR}/kernel_version
+
+# cat current kernel release version
+KERNEL_UTS=$(cat "${DIR}/include/generated/utsrelease.h" | awk '{print $3}' | \
+sed 's/\"//g' )
+
+echo "uname_r=$KERNEL_UTS" > ${DIR}/deploy/boot/uEnv.txt
+
+if [ -f "${DIR}/deploy/boot/${vmlinuz}-${KERNEL_UTS}" ]; then
+	rm -rf "${DIR}/deploy/boot/${image}-${KERNEL_UTS}"
+fi
+
+if [ -f ./arch/${KERNEL_ARCH}/boot/${image} ] ; then
+	cp -v arch/${KERNEL_ARCH}/boot/${image} \
+"${DIR}/deploy/boot/${vmlinuz}-${KERNEL_UTS}"
+fi
+
+echo "-----------------------------"
+echo "Installing kernel modules and copy dtbs.."
+
+make -s ARCH="${KERNEL_ARCH}" CROSS_COMPILE="${CC}" modules_install \
+INSTALL_MOD_PATH="${DIR}/deploy/"
+
+mkdir -p "${DIR}/deploy/boot/dtbs/${KERNEL_UTS}"
+cp ${DIR}/arch/${KERNEL_ARCH}/boot/dts/*.dtb \
+"${DIR}/deploy/boot/dtbs/${KERNEL_UTS}"
+
+rm -rf deploy/lib/firmware
+
+pkg="rootfs-update"
+deployfile="-${pkg}.tar.gz"
+tar_options="--create --gzip --file"
+echo "Compressing ${KERNEL_UTS}${deployfile}..."
+cd "${DIR}/deploy" || true
+tar ${tar_options} "../${KERNEL_UTS}${deployfile}" ./*
+cd ..
+
